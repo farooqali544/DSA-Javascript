3596f78d65cdcf6b66b79eef3d0777af0da5e3e9
Farooq Ali <farooqali544555@gmail.com>
2023-07-08 01:09:36 +0500

    Add mergeSortedArray Solution

---
+++ Array/mergeSortedArrays.js
@@ -0,0 +1,24 @@
+function mergeSortedArrays(arr1, arr2) {
+  const mergedArray = [];
+  let array1Item = arr1[0];
+  let array2Item = arr2[0];
+  let i = 1;
+  let j = 1;
+
+  while (array1Item || array2Item) {
+    if (!array2Item || array1Item < array2Item) {
+      mergedArray.push(array1Item);
+      array1Item = arr1[i];
+      i++;
+    } else {
+      mergedArray.push(array2Item);
+      array2Item = arr2[j];
+      j++;
+    }
+  }
+
+  console.log(mergedArray);
+  return mergedArray;
+}
+
+module.exports = { mergeSortedArrays };
